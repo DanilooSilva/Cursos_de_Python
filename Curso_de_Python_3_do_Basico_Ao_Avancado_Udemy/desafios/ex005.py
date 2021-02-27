36e2c1a9cf90cd48c238035b839fe9a8ce4e1939
Danilo Gomes <dno.gomesps@gmail.com>
2021-02-26 22:11:42 -0300

    add

---
+++ Curso_de_Python_3_do_Basico_Ao_Avancado_Udemy/desafios/ex005.py
@@ -0,0 +1,61 @@
+"""
+   Faça um lista de tarefas com as seguintes opções:
+        adicionar tarefa
+        listar tarefas
+        opção de desfazer (a cada vez que chamarmos, desfaz a útima ação)
+        opção de refazer (a cada vez que chamarmos, refaz a última ação) 
+
+        ['Tarefa 1', 'Tarefa 2']
+        ['Tarefa 1'] <- Desfazer
+        ['Tarefa 1', 'Tarefa 2'] < - Refazer
+"""
+
+from time import sleep
+
+def add_tarefas(tarefa, lista):
+    lista.append(tarefa)
+
+
+def listar_tarefas(lista):
+    for tarefa in lista:
+        print(tarefa)
+        sleep(3)
+
+
+def desfazer(lista):
+    lista.pop()
+
+
+lista_tarefas = []
+lista_tarefas_bkp = []
+tarefa = ''
+
+
+while True:
+    print('*-*' * 30)
+    print('''Selecione uma Opção: 
+        1 - Adicionar uma Tarefa
+        2 - Listar tarefas
+        3 - Desfazer
+        4 - Refazer
+        0 - Sair
+    ''')
+    print('*-*' * 30)
+    opcao = input('Digite aqui: ')
+    sleep(3)
+    if int(opcao) == 0:
+        break
+    elif int(opcao) == 1:
+        tarefa = str(input('Digite a Tarefa: '))
+        add_tarefas(tarefa, lista_tarefas)
+        print('Tarefa adicionada.')
+    elif int(opcao) == 2:
+        listar_tarefas(lista_tarefas)
+    elif int(opcao) == 3:
+        lista_tarefas_bkp = lista_tarefas[:]
+        desfazer(lista_tarefas)
+    elif int(opcao) == 4:
+        add_tarefas(lista_tarefas_bkp[-1], lista_tarefas)
+    else:
+        print('Opção Inválida')
+
